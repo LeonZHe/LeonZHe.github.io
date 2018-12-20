795a4aa0b4b3210f63fc061703f120a77a7ca0d8
Zhuolun Leon He <zleonhe@gmail.com>
2018-12-20 16:19:40 +0800

    Update homepage

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,10 +3,10 @@ function loadInstaImage() {
         get: 'user',
         //tagName: 'awesome',
         userId: '6076412082',
-        accessToken: '6076412082.1677ed0.52721339d4544d3a86fe890acc96e41f',
+        accessToken: '6076412082.1677ed0.a19869db331646fab8cb9725fe4bb2c4',
         clientId: '02fa372b97c44c2d88d0b12100d76b19',
         sortBy: 'most-recent',
-        limit: 15,
+        limit: 20,
         resolution: 'standard_resolution',
     });
     feed.run();
